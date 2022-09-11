f029e0209cb65c5c7e3a825cee0231a5549c5e69
danielc48 <danielschoi48@gmail.com>
2022-09-10 19:49:08 -0700

    Added error handling

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const Ticket = require('./models/ticket');
 const engine = require('ejs-mate');
+const ExpressError = require('./utils/ExpressError')
+const catchAsync = require('./utils/catchAsync')
 
 main().catch(err => console.log(err));
 
@@ -21,49 +23,58 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.get('/', (req,res) => {
-    res.send('AT THE HOME PAGE')
+    res.render('tickets/home')
 })
 
-app.get('/tickets', async (req,res) => {
+app.get('/tickets', catchAsync(async (req,res) => {
   const tickets = await Ticket.find({});
   res.render('tickets/index', {tickets})
-})
+}))
 
 app.get('/tickets/new', (req,res) => {
   res.render('tickets/new')
 })
 
-app.post('/tickets', async (req,res) => {
-  const ticket = new Ticket(req.body.ticket)
-  await ticket.save();
-  res.redirect('/tickets')
-})
+app.post('/tickets', catchAsync(async (req,res) => {
+    if(!req.body.tickets) throw new ExpressError(400, 'Invalid Ticket Data')
+    const ticket = new Ticket(req.body.ticket)
+    await ticket.save();
+    res.redirect('/tickets')
+}))
 
-app.get('/tickets/:id', async (req,res) => {
+app.get('/tickets/:id', catchAsync(async (req,res) => {
   const { id } = req.params;
   const ticket = await Ticket.findById(id);
   res.render('tickets/show', {ticket})
-})
+}))
 
-app.get('/tickets/:id/edit', async (req,res) => {
+app.get('/tickets/:id/edit', catchAsync(async (req,res) => {
   const { id } = req.params;
   const ticket = await Ticket.findById(id);
   res.render('tickets/edit', {ticket})
-})
+}))
 
-app.put('/tickets/:id', async (req,res) => {
+app.put('/tickets/:id', catchAsync(async (req,res) => {
   const { id } = req.params;
   const ticket = await Ticket.findByIdAndUpdate(id, {...req.body.ticket})
   res.redirect(`/tickets/${ticket.id}`)
-})
+}))
 
-app.delete('/tickets/:id', async (req,res) => {
+app.delete('/tickets/:id', catchAsync(async (req,res) => {
   const { id } = req.params
   await Ticket.findByIdAndDelete(id);
   res.redirect('/tickets')
+}))
+
+app.all('*', (req,res,next) => {
+  next(new ExpressError(404, 'Page Not Found'))
 })
 
-
+app.use((err,req,res,next) => {
+  const {statusCode = 500} = err;
+  if(!err.message) err.message = 'Oh No, something went wrong!'
+  res.status(statusCode).render('error', {err})
+})
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!')
